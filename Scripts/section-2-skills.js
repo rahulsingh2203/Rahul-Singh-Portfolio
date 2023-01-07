301dbc636e4bc5ab57a7caccf5ceaf2adcba0e43
Rahul Singh <rahul.singh.ec.2017@miet.ac.in>
2023-01-07 21:45:42 +0530

    skills section completed

--- Scripts/section-2-skills.js
+++ Scripts/section-2-skills.js
@@ -2,27 +2,27 @@ let skillSlide = document.getElementsByClassName("section-2-container-subcontain
 let numss = 3;
 let ii = 0;
 function skillNext() {
-    if (ii < numss - 1) {
-        skillSlides[ii].classList.remove("active-skill");
-        skillSlides[ii + 1].classList.add("active-skill");
+    if (ii < 2) {
+        skillSlide[ii].classList.remove("active-skill");
+        skillSlide[ii + 1].classList.add("active-skill");
         ii++;
     }
     else {
-        skillSlides[ii].classList.remove("active-skill");
-        i = 0;
-        skillSlides[ii].classList.add("active-skill");
-        skillSlides[ii + 1].classList.remove("active-skill");
+        skillSlide[ii].classList.remove("active-skill");
+        ii = 0;
+        skillSlide[ii].classList.add("active-skill");
+        skillSlide[ii + 1].classList.remove("active-skill");
     }
 }
 function skillPrev() {
     if (ii > 0) {
-        skillSlides[ii].classList.remove("active-skill");
-        skillSlides[ii - 1].classList.add("active-skill");
+        skillSlide[ii].classList.remove("active-skill");
+        skillSlide[ii - 1].classList.add("active-skill");
         ii--;
     }
     else {
-        skillSlides[ii].classList.remove("active-skill");
-        skillSlides[ii + 2].classList.add("active-skill");
-        ii = numss - 1;
+        skillSlide[ii].classList.remove("active-skill");
+        skillSlide[ii + 2].classList.add("active-skill");
+        ii = 2;
     }
 }
